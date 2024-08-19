a16aa0c1e7972c2ee0e359fa423a0c6c0b5b166c
EOkten <eduardokten@gmail.com>
2024-08-19 22:05:25 +0300

    lesson 8 (clean up)

--- src/main/java/org/okten/demo/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package org.okten.demo.repository;
-
-import org.okten.demo.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
